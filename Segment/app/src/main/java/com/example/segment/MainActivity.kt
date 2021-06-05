efd4bc21a511230beb692299e20f9d9593a15344
Dahyunne <ryudh98@gmail.com>
2021-06-06 00:41:26 +0900

    화면 전환 구현

--- Segment/app/src/main/java/com/example/segment/MainActivity.kt
+++ Segment/app/src/main/java/com/example/segment/MainActivity.kt
@@ -1,15 +1,50 @@
 package com.example.segment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.segment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //로그인 후 이동하는 메인 화면
     //유저정보 볼수있는 화면으로 갈수있다
     //큰채팅방 목록 볼수있는 화면으로도 갈수있다
-    //로그아웃도 할수있다 와!!
+    //로그아웃도 할수있다
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var actionBar : ActionBar?
+        actionBar = supportActionBar
+        actionBar?.hide()
+
+        init()
+
+    }
+
+    private fun init() {
+        //유저 정보 받아온것
+        binding.apply {
+            btChat.setOnClickListener {
+                //큰채팅방 목록으로
+                intent = Intent(this@MainActivity, MainchatlistActivity::class.java)
+                startActivity(intent)
+            }
+            btUser.setOnClickListener {
+                //유저화면으로
+                intent = Intent(this@MainActivity, UserActivity::class.java)
+                startActivity(intent)
+            }
+            btLogout.setOnClickListener {
+                //로그아웃
+                intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- Segment/app/src/main/java/com/example/segment/MainchatlistActivity.kt
+++ Segment/app/src/main/java/com/example/segment/MainchatlistActivity.kt
@@ -1,12 +1,30 @@
 package com.example.segment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.segment.databinding.ActivityMainchatlistBinding
 
 class MainchatlistActivity : AppCompatActivity() {
     //메인 채팅방 목록 보이는 화면
+    lateinit var binding: ActivityMainchatlistBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mainchatlist)
+        binding = ActivityMainchatlistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var actionBar : ActionBar?
+        actionBar = supportActionBar
+        actionBar?.hide()
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            button2.setOnClickListener {
+                intent = Intent(this@MainchatlistActivity,MainchatroomActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
