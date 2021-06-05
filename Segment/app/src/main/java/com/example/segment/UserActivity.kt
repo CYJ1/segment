1227fce1fa25abe0b5027f21312bcb6c84067940
Dahyunne <ryudh98@gmail.com>
2021-06-06 00:44:14 +0900

    설명이랑 글씨키움(user)

--- Segment/app/src/main/java/com/example/segment/UserActivity.kt
+++ Segment/app/src/main/java/com/example/segment/UserActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 
 class UserActivity : AppCompatActivity() {
+    //유저의 로그인 상태를 확인할 수 있는 화면
+    //userstatus.row에 잇는 isOnline image 색상으로 구분가능
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
